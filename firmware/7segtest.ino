bbb7ee896e75e6a125ef310d929c389e93fabffc
Meghana Madiraju <meghanambpd@gmail.com>
2025-05-09 20:14:58 -0400

    trying firmware stuff

---
+++ firmware/7segtest.ino
@@ -0,0 +1,53 @@
+/*
+  Showing number 0-9 on a Common Anode 7-segment LED display
+  Displays the numbers 0-9 on the display, with one second inbetween.
+    A
+   ---
+F |   | B
+  | G |
+   ---
+E |   | C
+  |   |
+   ---
+    D
+  This example code is in the public domain.
+ */
+ 
+// Pin 2-8 is connected  to the 7 segments of the display.
+
+#include <SevSeg.h>
+SevSeg = sevseg;
+
+int pinA = 2;
+int pinB = 3;
+int pinC  = 4;
+int pinD = 5;
+int pinE = 6;
+int pinF = 7;
+int pinG = 8;
+int D1 = 9;
+int D2 = 10;
+int D3 = 11;
+int D4 = 12;
+
+// the setup routine  runs once when you press reset:
+void setup() {                
+  // initialize  the digital pins as outputs.
+  pinMode(pinA, OUTPUT);     
+  pinMode(pinB,  OUTPUT);     
+  pinMode(pinC, OUTPUT);     
+  pinMode(pinD, OUTPUT);     
+  pinMode(pinE, OUTPUT);     
+  pinMode(pinF, OUTPUT);     
+  pinMode(pinG,  OUTPUT);   
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);  
+  pinMode(D3,  OUTPUT);  
+  pinMode(D4, OUTPUT);  
+}
+
+void loop() {
+  digitalWrite(D1, HIGH);
+  digitalWrite(pinA, HIGH);
+
+}
