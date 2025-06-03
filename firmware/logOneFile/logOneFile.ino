8c37f036bab803c727560078ee6749d1c57cb376
MeghanaM4 <meghanaamadiraju@gmail.com>
2025-06-03 16:57:21 -0400

    plotting and logging sensor data !!

--- firmware/logOneFile/logOneFile.ino
+++ firmware/logOneFile/logOneFile.ino
@@ -1,6 +1,15 @@
 //open Serial Plotter and plot
 
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+//microSD card
+const int chipSelect = 29;
+
+//for time
+long startTime = 0;
+
 
 //MS5607 altimeter
 #define MS5607_ADDR 0x77
@@ -34,14 +43,22 @@ float magX = 0, magY = 0, magZ = 0, magMagnitude = 0;
 
 void setup() {
   Serial.begin(115200);
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+  Serial.println("SD card initialized.");
+
   Wire.begin();
-  
-  //initialize sensors
+
+
   initMS5607();
   initMPU6050();
   initALS31313();
-  
+
   delay(2000);
+  startTime = millis();
 }
 
 void loop() {
@@ -49,29 +66,31 @@ void loop() {
   readMS5607();
   readMPU6050();
   readALS31313();
+
+  File datalog = SD.open("datalog.csv", FILE_WRITE);
+
+  if(datalog) {
+    datalog.print(millis() - startTime); datalog.print(",");
+    datalog.print(temperature); datalog.print(",");
+    datalog.print(pressure); datalog.print(",");
+    datalog.print(altitude); datalog.print(",");
+    datalog.print(accelX); datalog.print(",");
+    datalog.print(accelY); datalog.print(",");
+    datalog.print(accelZ); datalog.print(",");
+    datalog.print(gyroX); datalog.print(",");
+    datalog.print(gyroY); datalog.print(",");
+    datalog.print(gyroZ); datalog.print(",");
+    datalog.print(magX); datalog.print(",");
+    datalog.print(magY); datalog.print(",");
+    datalog.print(magZ); datalog.print(",");
+    datalog.println(magMagnitude);
+  } else {
+    Serial.println("Error opening datalog file");
+  }
   
-  //Serial Plotter format stuff
-  Serial.print("Temperature:"); Serial.print(temperature); Serial.print(",");
-  Serial.print("Pressure:"); Serial.print(pressure); Serial.print(",");
-  Serial.print("Altitude:"); Serial.print(altitude); Serial.print(",");
-  Serial.print("AccelX:"); Serial.print(accelX); Serial.print(",");
-  Serial.print("AccelY:"); Serial.print(accelY); Serial.print(",");
-  Serial.print("AccelZ:"); Serial.print(accelZ); Serial.print(",");
-  Serial.print("Rotation X:"); Serial.print(gyroX); Serial.print(",");
-  Serial.print("Rotation Y:"); Serial.print(gyroY); Serial.print(",");
-  Serial.print("Rotation Z:"); Serial.print(gyroZ); Serial.print(",");
-  Serial.print("MagX:"); Serial.print(magX); Serial.print(",");
-  Serial.print("MayY:"); Serial.print(magY); Serial.print(",");
-  Serial.print("MagZ:"); Serial.print(magZ); Serial.print(",");
-  Serial.print("Magnetic Magnitude:"); Serial.println(magMagnitude);
   
-  delay(100); 
+  delay(100);
 }
-
-
-
-
-
 
 //===== MS5607 Functions =====
 bool initMS5607() {
@@ -79,12 +98,12 @@ bool initMS5607() {
   Wire.write(CMD_RESET);
   if (Wire.endTransmission() != 0) return false;
   delay(10);
-  
+
   for (int i = 0; i < 8; i++) {
     Wire.beginTransmission(MS5607_ADDR);
     Wire.write(CMD_PROM_READ + (i * 2));
     Wire.endTransmission();
-    
+
     Wire.requestFrom(MS5607_ADDR, 2);
     if (Wire.available() >= 2) {
       C[i] = (Wire.read() << 8) | Wire.read();
@@ -98,15 +117,15 @@ bool initMS5607() {
 void readMS5607() {
   uint32_t rawPressure = readRawPressure();
   uint32_t rawTemp = readRawTemperature();
-  
+
   int32_t dT = rawTemp - ((uint32_t)C[5] << 8);
   int32_t TEMP = 2000 + (((int64_t)dT * C[6]) >> 23);
-  
+
   int64_t OFF = ((int64_t)C[2] << 17) + (((int64_t)C[4] * dT) >> 6);
   int64_t SENS = ((int64_t)C[1] << 16) + (((int64_t)C[3] * dT) >> 7);
-  
+
   int32_t P = (((rawPressure * SENS) >> 21) - OFF) >> 15;
-  
+
   temperature = TEMP / 100.0;
   pressure = P / 100.0;
   altitude = 44330.0 * (1.0 - pow(pressure / 1013.25, 0.1903));
@@ -117,11 +136,11 @@ uint32_t readRawPressure() {
   Wire.write(CMD_CONVERT_D1_4096);
   Wire.endTransmission();
   delay(10);
-  
+
   Wire.beginTransmission(MS5607_ADDR);
   Wire.write(CMD_ADC_READ);
   Wire.endTransmission();
-  
+
   Wire.requestFrom(MS5607_ADDR, 3);
   uint32_t result = 0;
   if (Wire.available() >= 3) {
@@ -135,11 +154,11 @@ uint32_t readRawTemperature() {
   Wire.write(CMD_CONVERT_D2_4096);
   Wire.endTransmission();
   delay(10);
-  
+
   Wire.beginTransmission(MS5607_ADDR);
   Wire.write(CMD_ADC_READ);
   Wire.endTransmission();
-  
+
   Wire.requestFrom(MS5607_ADDR, 3);
   uint32_t result = 0;
   if (Wire.available() >= 3) {
@@ -148,11 +167,15 @@ uint32_t readRawTemperature() {
   return result;
 }
 
+
+
+
+
 //===== MPU6050 Functions =====
 bool initMPU6050() {
   Wire.beginTransmission(MPU6050_ADDR);
   if (Wire.endTransmission() != 0) return false;
-  
+
   Wire.beginTransmission(MPU6050_ADDR);
   Wire.write(MPU6050_PWR_MGMT_1);
   Wire.write(0);
@@ -164,29 +187,28 @@ void readMPU6050() {
   Wire.write(MPU6050_ACCEL_XOUT_H);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU6050_ADDR, 14, true);
-  
+
   if (Wire.available() >= 14) {
     int16_t axRaw = Wire.read() << 8 | Wire.read();
     int16_t ayRaw = Wire.read() << 8 | Wire.read();
     int16_t azRaw = Wire.read() << 8 | Wire.read();
-    
-    Wire.read(); Wire.read(); //Skip temperature readings
-    
+
+    Wire.read();
+    Wire.read();  //Skip temperature readings
+
     int16_t gxRaw = Wire.read() << 8 | Wire.read();
     int16_t gyRaw = Wire.read() << 8 | Wire.read();
     int16_t gzRaw = Wire.read() << 8 | Wire.read();
-    
+
     accelX = axRaw / 16384.0;
     accelY = ayRaw / 16384.0;
     accelZ = azRaw / 16384.0;
-    
+
     gyroX = gxRaw / 131.0;
     gyroY = gyRaw / 131.0;
     gyroZ = gzRaw / 131.0;
   }
 }
-
-
 
 
 
@@ -201,28 +223,28 @@ void readALS31313() {
   int16_t magXRaw = readALS31313Register(ALS31313_X_MSB, ALS31313_X_LSB);
   int16_t magYRaw = readALS31313Register(ALS31313_Y_MSB, ALS31313_Y_LSB);
   int16_t magZRaw = readALS31313Register(ALS31313_Z_MSB, ALS31313_Z_LSB);
-  
+
   magX = magXRaw * 0.0001;
   magY = magYRaw * 0.0001;
   magZ = magZRaw * 0.0001;
-  
-  magMagnitude = sqrt(magX*magX + magY*magY + magZ*magZ);
+
+  magMagnitude = sqrt(magX * magX + magY * magY + magZ * magZ);
 }
 
 int16_t readALS31313Register(uint8_t regMSB, uint8_t regLSB) {
   uint8_t msb = 0, lsb = 0;
-  
+
   Wire.beginTransmission(ALS31313_ADDR);
   Wire.write(regMSB);
   Wire.endTransmission();
   Wire.requestFrom(ALS31313_ADDR, 1);
   if (Wire.available()) msb = Wire.read();
-  
+
   Wire.beginTransmission(ALS31313_ADDR);
   Wire.write(regLSB);
   Wire.endTransmission();
   Wire.requestFrom(ALS31313_ADDR, 1);
   if (Wire.available()) lsb = Wire.read();
-  
+
   return (int16_t)((msb << 8) | lsb);
 }
